168eb663b9c39e2695e741a359e056bfc18b4baf
TeamMeng <862915265@qq.com>
2024-09-16 18:02:59 +0800

    feature: refactor code to a workspace and start notify server

---
+++ notify_server/src/main.rs
@@ -0,0 +1,29 @@
+use anyhow::Result;
+use notify_server::get_router;
+use tokio::net::TcpListener;
+use tracing::{info, level_filters::LevelFilter};
+use tracing_subscriber::{
+    fmt::{format::FmtSpan, Layer},
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+    Layer as _,
+};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let layer = Layer::new()
+        .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
+        .pretty()
+        .with_filter(LevelFilter::INFO);
+    tracing_subscriber::registry().with(layer).init();
+
+    let addr = "0.0.0.0:6687";
+    info!("Server listening on {}", addr);
+    let listener = TcpListener::bind(addr).await?;
+
+    let app = get_router();
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}

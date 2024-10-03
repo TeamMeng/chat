a30c72dbf44ad036ca58fea101a99ac4904a2796
TeamMeng <862915265@qq.com>
2024-10-03 10:34:40 +0800

    fix: limit should have default and all data should user camelCase

--- chat_core/src/lib.rs
+++ chat_core/src/lib.rs
@@ -33,6 +33,7 @@ pub struct Workspace {
 }
 
 #[derive(Debug, Clone, ToSchema, FromRow, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
 pub struct ChatUser {
     pub id: i64,
     pub fullname: String,
@@ -40,6 +41,7 @@ pub struct ChatUser {
 }
 
 #[derive(Debug, Clone, ToSchema, FromRow, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
 pub struct Chat {
     pub id: i64,
     pub ws_id: i64,
@@ -51,7 +53,7 @@ pub struct Chat {
 
 #[derive(Debug, Clone, ToSchema, Serialize, Deserialize, PartialEq, PartialOrd, sqlx::Type)]
 #[sqlx(type_name = "chat_type", rename_all = "snake_case")]
-#[serde(rename_all = "snake_case")]
+#[serde(rename_all = "camelCase")]
 pub enum ChatType {
     Single,
     Group,
@@ -60,6 +62,7 @@ pub enum ChatType {
 }
 
 #[derive(Debug, Clone, ToSchema, FromRow, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "camelCase")]
 pub struct Message {
     pub id: i64,
     pub chat_id: i64,
